aaee412ff67c6943c3d36f6cbee5e1c11d0850ec
Nihar04 <69451996+Nihar04@users.noreply.github.com>
2022-11-13 22:44:07 +0530

    Update if&else_statements2.c

--- if&else_statements2.c
+++ if&else_statements2.c
@@ -296,3 +296,25 @@ int main(){
         printf("The employee has recieved a bonus of Rs. 2,500/-");
     }
 }
+
+/*slope of a line*/
+#include <stdio.h>
+int main()
+{
+float x1, y1, x2, y2, x3, y3, m, n;
+printf("Enter points (x1, y1)\n"); scanf("%f%f", &x1, &y1);
+printf("Enter points (x2, y2)\n");
+scanf("%f%f", &x2, &y2);
+printf("Enter points (x3, y3)\n");
+scanf("%f%f", &x3, &y3);
+m n =(y2 y1) / (x2 - x1); = (y3 y2)/(x3 - x2);
+if( m == n)
+{
+}
+else
+{
+printf("All 3 points do not lie on the same line\n");
+}
+printf("All 3 points lie on the same line\n");
+return 0;
+}
